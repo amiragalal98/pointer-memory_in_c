df0617c3f9c3e9b161cefb962fef00a02937bb21
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-04-06 00:15:12 +0200

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+  // Checkpoint 1 code goes here.
+double dblVar;
+double* dblPtr = &dblVar;
+  // Print the address stored in dblPtr
+printf("%p", dblPtr);
+  
+}
